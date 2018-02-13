de28653d43f36b67304192813dd6568e4f05a76f
Sultan Saadat <sultansaadat@gmail.com>
2018-02-13 13:48:48 +0200

    title change again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CB Node Starter' });
+  res.render('index', { title: 'CB Node Starter v1.0' });
 });
 
 module.exports = router;

df358608926b5c3f75f832e1f98a24d7c152110e
Sultan Saadat <sultansaadat@gmail.com>
2018-02-13 16:52:22 +0200

    bot route

---
+++ routes/bot.js
@@ -0,0 +1,12 @@
+// function(req, res) {
+//    
+
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/bot', function(req, res, next) {
+   fs.readFile('./bot.json', { encoding: 'utf8' }, function(err, data) {
+        console.log(data);
+        res.send(data);
+    });
+  
 });
 
 module.exports = router;

a32d5c5e2ce780e7d78816f5ff890c4448e38af2
Sultan Saadat <sultansaadat@gmail.com>
2018-03-10 17:04:08 +0400

    cluster data

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var request = require('request');
 /* GET users listing. */
 router.get('/bot', function(req, res, next) {
-   fs.readFile('./bot.json', { encoding: 'utf8' }, function(err, data) {
+    fs.readFile('./bot.json', { encoding: 'utf8' }, function(err, data) {
         console.log(data);
         res.send(data);
     });
-  
+
 });
 
+router.get('/cluster-data', function(req, res, next) {
+    request('http://leader.mesos:8080/v2/info', function(error, response, body) {
+        res.send(body);
+    });
+})
 module.exports = router;
